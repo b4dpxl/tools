c7e77c1f4ec75a596ce547cc447fa8d0b662e3b9
b4dpxl <b4d@pxl.me.uk>
2017-11-24 13:28:27 +0000

    Updated 'selector' regex

--- dkim_key_length_check.py
+++ dkim_key_length_check.py
@@ -40,7 +40,7 @@ def extract_headers( file ):
         sys.exit(-3)
     if "DKIM-Signature" in header:
         print( "Got header" )
-        s = re.search( """\\bs=(\\w+);""", header ).group(1)
+        s = re.search( """\\bs=([\\w\\-]+);""", header ).group(1)
         d = re.search( """\\bd=(([\\w+\\-]+\\.)+[\\w+\\-]+\\w+);""", header ).group(1)
         return (s, d)
     else:
