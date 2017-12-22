f44bbb17936a50fd722b1b450d91c1cdc8305d92
b4dpxl <b4d@pxl.me.uk>
2017-12-22 13:15:52 +0000

    added debug method

--- __printer.py
+++ __printer.py
@@ -8,6 +8,11 @@ class printer:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+    _debug = False
+
+    def __init__(self, debug=True):
+        self._debug = debug
+
     def ok(self, str):
         self.print_col( "[+]", str, self.OKGREEN)
 
@@ -15,10 +20,14 @@ class printer:
         self.print_col( "[*]", str, self.OKBLUE)
 
     def warn(self, str):
-        self.print_col( "[-]", str, self.WARNING)
+        self.print_col( "[~]", str, self.WARNING)
 
     def error(self, str):
         self.print_col( "[!]", str, self.FAIL)
+
+    def debug(self, str):
+        if self._debug:
+            self.__default("[-] %s" % str)
 
     def print_col(self, str1, str2, col):
         print("%s%s%s %s" % (col, str1, self.ENDC, str2))
