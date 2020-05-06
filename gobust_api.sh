3e78fd4964f9572e4f08cae6eb5d9334bd33a8ae
B4dpxl <b4d@pxl.me.uk>
2020-05-06 14:02:37 +0100

    Rex update

--- gobust_api.sh
+++ gobust_api.sh
@@ -10,7 +10,7 @@ URL=$2
 
 if [ -z "$URL" ] || [ -z "$WL" ] || [ ! -d "$WL" ]; then usage ; fi
 
-out=gobuster-`echo "$URL" | sed -r 's/^https?:\/\///' |sed -r 's/\W+/_/g'`.txt
+out=gobuster-`echo "$URL" | sed -r 's/^https?:\/\///' |sed -r 's/[^a-zA-Z0-9\-]+/_/g'`.txt
 
 for w in /pentest_d/wordlists/api_wordlist/*.txt ; do 
     gobuster dir -w "$w" -t 3 -u "$URL"  -s "200,204,301,302,307,401,403,405" 
